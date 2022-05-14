d43db26b399d559968377958f179cbccf5bdb54b
Ubuntu <ubuntu@cmip6-crunching.novalocal>
2022-05-14 01:08:03 +0000

    Add fix for ACCESS ESM1.5

--- esmvalcore/cmor/_fixes/cmip6/access_esm1_5.py
+++ esmvalcore/cmor/_fixes/cmip6/access_esm1_5.py
@@ -6,6 +6,35 @@ from ..common import ClFixHybridHeightCoord
 from ..fix import Fix
 
 
+class AllVars(Fix):
+    """Fixes for all vars."""
+
+    def fix_metadata(self, cubes):
+        """Fix metadata
+
+        Parameters
+        ----------
+        cubes : iris.cube.CubeList
+            Input cubes.
+
+        Returns
+        -------
+        iris.cube.CubeList
+
+        """
+        for cube in cubes:
+            if cube.attributes.get('experiment_id', '') == 'ssp534-over':
+                branch_time_in_parent = "branch_time_in_parent"
+                bad_value = 60265.0
+                for cube in cubes:
+                    try:
+                        if cube.attributes[branch_time_in_parent] == bad_value:
+                            cube.attributes[branch_time_in_parent] = 69762.0
+                    except AttributeError:
+                        pass
+        return cubes
+
+
 class Cl(ClFixHybridHeightCoord):
     """Fixes for cl."""
 
