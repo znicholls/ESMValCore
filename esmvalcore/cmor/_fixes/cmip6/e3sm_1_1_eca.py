d030c5dbe4c61026f708fde6c6c86c81a55d0aaa
Ubuntu <ubuntu@cmip6-crunching.novalocal>
2022-05-15 22:03:24 +0000

    Add E3SM-1-1* fixes

---
+++ esmvalcore/cmor/_fixes/cmip6/e3sm_1_1_eca.py
@@ -0,0 +1,31 @@
+"""Fixes for E3SM-1-1-ECA model."""
+from ..fix import Fix
+
+class AllVars(Fix):
+    """Fixes for all vars."""
+
+    def fix_metadata(self, cubes):
+        """Fix metadata
+
+        Parameters
+        ----------
+        cubes : iris.cube.CubeList
+            Input cubes.
+
+        Returns
+        -------
+        iris.cube.CubeList
+
+        """
+        for cube in cubes:
+            if cube.attributes.get('experiment_id', '').startswith("ssp"):
+                branch_time_in_parent = "branch_time_in_parent"
+                bad_value = 0.0
+                for cube in cubes:
+                    try:
+                        if cube.attributes[branch_time_in_parent] == bad_value:
+                            cube.attributes[branch_time_in_parent] = 60265.
+                    except AttributeError:
+                        pass
+
+        return cubes
